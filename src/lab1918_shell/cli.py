c44f7c941f6ac2b1a43c4f36833e5d2e92f7194a
Eric Dong <igp2bgp@gmail.com>
2023-12-16 19:08:09 -0800

    add patch and delete method for topology client

--- src/lab1918_shell/cli.py
+++ src/lab1918_shell/cli.py
@@ -31,15 +31,48 @@ def create(ctx, topology_name):
 
 @topology.command()
 @click.pass_context
-def list(ctx):
+@click.option("--topology-id", help="topology id")
+def list(ctx, topology_id):
     client = ctx.obj["client"]
-    logger.info("list all topologies ...")
+    logger.info("list topologies ...")
     try:
-        res = client.get_all_topologies()
+        if topology_id:
+            res = client.get_topology(topology_id)
+        else:
+            res = client.get_all_topologies()
         res.raise_for_status()
         click.echo(json.dumps(res.json(), indent=4))
     except Exception as e:
         click.echo(f"failed to create topology: {e}", err=True)
+
+
+@topology.command()
+@click.pass_context
+@click.option("--topology-id", help="topology id")
+def delete(ctx, topology_id):
+    client = ctx.obj["client"]
+    logger.info("delete topology ...")
+    try:
+        res = client.delete_topology(topology_id)
+        res.raise_for_status()
+        click.echo(f"deleted topology {topology_id}")
+    except Exception as e:
+        click.echo(f"failed to delete topology: {e}", err=True)
+
+
+@topology.command()
+@click.pass_context
+@click.option("--topology-id", help="topology id")
+@click.option("--topology-name", help="topology name")
+def update(ctx, topology_id, topology_name):
+    client = ctx.obj["client"]
+    logger.info("update topology ...")
+    try:
+        res = client.update_topology(topology_id, topology_name)
+        res.raise_for_status()
+        click.echo(json.dumps(res.json(), indent=4))
+    except Exception as e:
+        click.echo(f"failed to update topology: {e}", err=True)
 
 
 def main():
